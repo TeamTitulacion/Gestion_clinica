2b65e2255d0cd8a1471f291dad978917f621288c
IsmaelPineida <anibalismaelpineidaflores@gmail.com>
2022-03-07 01:01:16 -0500

    url

--- core/ConfigGeneral.php
+++ core/ConfigGeneral.php
@@ -1,5 +1,5 @@
 <?php
-const SERVERURL = 'http://localhost/Gestion_clinica-main';
+const SERVERURL = 'http://localhost/Gestion_clinica';
 const COMPANY   = 'Cruz Medi Dental';
 const direccion = '';
 date_default_timezone_set('America/Guayaquil');
