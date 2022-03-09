3eccd9b8c579d6806eddc10718a74a8a879dc4da
Gabo2108 <death-03@hotmail.es>
2022-03-09 17:50:23 -0500

    dashboard & views

--- controlador/visitas.controlador.php
+++ controlador/visitas.controlador.php
@@ -4,40 +4,38 @@ if ($peticionAjax) {
 } else {
     require_once "./modelo/visitas.modelo.php";
 }
-class VisitasControlador extends VisitasModelo{
-    public function CtrVisitas(){
-        $ip = getRealIP();
-        function getRealIP() {
-
-            if (!empty($_SERVER['HTTP_CLIENT_IP']))
-                return $_SERVER['HTTP_CLIENT_IP'];
-               
-            if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-                return $_SERVER['HTTP_X_FORWARDED_FOR'];
-           
-            return $_SERVER['REMOTE_ADDR'];
+class VisitasControlador extends VisitasModelo
+{
+    public function CtrVisitas()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
         }
         $visita = VisitasModelo::MdlVistas($ip);
-         
+
         if ($visita->rowCount() == 0) {
-            $fecha= date("y-m-d H:i:s");
-            $dato=['ip'=>$ip,'fecha'=>$fecha];
+            $fecha = date("y-m-d H:i:s");
+            $dato = ['ip' => $ip, 'fecha' => $fecha];
             $insertarip = VisitasModelo::MdlInsertar($dato);
-        }else{
-            $dato= $visita->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            $dato = $visita->fetchAll(PDO::FETCH_ASSOC);
             foreach ($dato as $key => $value) {
-               
-                    $array= $value['vis_fecha'];
-                
+
+                $array = $value['vis_fecha'];
             }
             $fechaRegistro = $array;
-            $fechaActual= date('Y-m-d H:i:s');
-            $nuevaFecha= strtotime($fechaRegistro.'+ 5 hours');
-            $nuevaFecha= date('Y-m-d H:i:s',$nuevaFecha);
-            $dato=['ip'=>$ip,'fecha'=>$nuevaFecha];
+            $fechaActual = date('Y-m-d H:i:s');
+            $nuevaFecha = strtotime($fechaRegistro . '+ 5 hours');
+            $nuevaFecha = date('Y-m-d H:i:s', $nuevaFecha);
+            $dato = ['ip' => $ip, 'fecha' => $nuevaFecha];
             if ($fechaActual >= $nuevaFecha) {
                 $insertarip = VisitasModelo::MdlInsertar($dato);
-            } 
+            }
         }
-        }
+    }
 }
